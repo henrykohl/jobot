e915d5b6bb3d5dea2fe9f9c4feb5b6c500fb2fc6
Henry Hsu <43148403+henrykohl@users.noreply.github.com>
2024-12-10 17:25:24 +0000

    add Head

--- jobot-web/pages/index.js
+++ jobot-web/pages/index.js
@@ -14,6 +14,7 @@
 
 import { useState } from "react";
 import ReactMarkdown from 'react-markdown'
+import Head from "next/head";
 
 const SYSTEM_MESSAGE = "You are Jobot, a helpful and verstaile AI developed by Jovian using state-of-the-art ML models. and APIs."
 
@@ -92,81 +93,86 @@ export default function Home() {
 }
   
   return (
-    <div className="flex flex-col h-screen">
-      {/* Navigation Bar */}
-      <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
-        <div className="text-xl font-bold">Job</div>
-        <div>
-          <input
-                type="password"
-                className="border rounded p-1"
-                placeholder="Enter API key.."
-                value={apiKey}
-                onChange={(e) => setApiKey(e.target.value)}
-              />
-        </div>
-      </nav>
-      
-      {/* Message History */}
-      <div className="flex-1 overflow-y-scroll">
-        <div className="mx-auto w-full max-w-screen-md p-4 ">
-          {/* 初版 */}
-          {/* {messages.map((message, idx) => (
-            <div key={idx} className="mt-3">
-              <div className="font-bold">{message.role}</div>
-              <div className="text-lg">{message.content}</div>
-            </div>
-          ))} */}
-          {/* 改版 */}
-          {messages
-          .filter((message) => message.role !== "system")
-          .map((message, idx) => (
-            <div key={idx} className="my-3">
-              {/* 初始 */}
-              {/* <div className="font-bold">{message.role}</div>
-              <div className="text-lg">{message.content}</div> */}
-              {/* 修改過 */}
-              <div className="font-bold">
-                {message.role === "user" ? "You" : "Jobot"}
-              </div>
-              <div className="text-lg prose">
-                <ReactMarkdown>{message.content}</ReactMarkdown>
-              </div>
-            </div>
-          ))}
-        </div>
+    <>
+      <Head>
+          <title>Jobot</title>
+      </Head>
+      <div className="flex flex-col h-screen">
+        {/* Navigation Bar */}
+        <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
+          <div className="text-xl font-bold">Job</div>
+          <div>
+            <input
+                  type="password"
+                  className="border rounded p-1"
+                  placeholder="Enter API key.."
+                  value={apiKey}
+                  onChange={(e) => setApiKey(e.target.value)}
+                />
+          </div>
+        </nav>
         
-      </div>
+        {/* Message History */}
+        <div className="flex-1 overflow-y-scroll">
+          <div className="mx-auto w-full max-w-screen-md p-4 ">
+            {/* 初版 */}
+            {/* {messages.map((message, idx) => (
+              <div key={idx} className="mt-3">
+                <div className="font-bold">{message.role}</div>
+                <div className="text-lg">{message.content}</div>
+              </div>
+            ))} */}
+            {/* 改版 */}
+            {messages
+            .filter((message) => message.role !== "system")
+            .map((message, idx) => (
+              <div key={idx} className="my-3">
+                {/* 初始 */}
+                {/* <div className="font-bold">{message.role}</div>
+                <div className="text-lg">{message.content}</div> */}
+                {/* 修改過 */}
+                <div className="font-bold">
+                  {message.role === "user" ? "You" : "Jobot"}
+                </div>
+                <div className="text-lg prose">
+                  <ReactMarkdown>{message.content}</ReactMarkdown>
+                </div>
+              </div>
+            ))}
+          </div>
+          
+        </div>
 
-      {/* Message Input Box */}
-      <div>
-        <div className="mx-auto w-full max-w-screen-md flex px-4 pb-4">
-          <textarea
-            value={userMessage}
-            // onChange={handleTyping}
-            onChange={(e)=>setUserMessage(e.target.value)} // 取代 handleTyping
-            className="border rounded-md text-lg p-1 flex-1"
-            rows={1}
-          />
+        {/* Message Input Box */}
+        <div>
+          <div className="mx-auto w-full max-w-screen-md flex px-4 pb-4">
+            <textarea
+              value={userMessage}
+              // onChange={handleTyping}
+              onChange={(e)=>setUserMessage(e.target.value)} // 取代 handleTyping
+              className="border rounded-md text-lg p-1 flex-1"
+              rows={1}
+            />
+            <button
+              onClick={sendRequest}
+              className="border rounded-md bg-blue-500 hover:bg-blue-600 text-white text-lg p-1 w-20 ml-2"
+            >
+              Send
+            </button>
+          </div>
+        </div>
+        {/* <div className="p-4">
           <button
             onClick={sendRequest}
-            className="border rounded-md bg-blue-500 hover:bg-blue-600 text-white text-lg p-1 w-20 ml-2"
+            // className="w-40 bordered rounded bg-blue-500 hover:bg-blue-600 text-white p-2"
+            className="bordered rounded-md bg-blue-500 hover:bg-blue-600 text-white p-2"
           >
-            Send
+            Send Request
           </button>
-        </div>
+        <div className="mt-4 text-lg">{botMessage}</div>
+        </div> */}
       </div>
-      {/* <div className="p-4">
-        <button
-          onClick={sendRequest}
-          // className="w-40 bordered rounded bg-blue-500 hover:bg-blue-600 text-white p-2"
-          className="bordered rounded-md bg-blue-500 hover:bg-blue-600 text-white p-2"
-        >
-          Send Request
-        </button>
-      <div className="mt-4 text-lg">{botMessage}</div>
-      </div> */}
-    </div>
+    </>
   );
 }
 
