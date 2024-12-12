6c65973b2adf41ce92f743ca9a89bb0783454bea
TAYUAN <u860218@gmail.com>
2024-12-12 04:42:24 -0600

    file uploading

---
+++ backup-files/index_v2-1.js
@@ -0,0 +1,87 @@
+import { useState } from "react";
+// import ReactMarkdown from 'react-markdown'
+// import Head from "next/head";
+// import { createParser } from "eventsource-parser";
+
+const SYSTEM_MESSAGE =
+  "You are Jobot, a helpful and verstaile AI developed by Jovian using state-of-the-art ML models. and APIs.";
+
+export default function Home() {
+  const [apiKey, setApiKey] = useState("");
+
+  const [messageHistory, setMessageHistory] = useState([
+    { role: "system", content: SYSTEM_MESSAGE },
+    { role: "user", content: "What is JavaScript?" },
+  ]);
+
+  const [userMessage, setUserMessage] = useState("");
+
+  const API_URL = "https://api.openai.com/v1/chat/completions";
+
+  async function sendRequest() {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + apiKey,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          { role: "system", content: SYSTEM_MESSAGE },
+          // {"role": "user", "content": "Hello, please introduce yourself!"}
+          { role: "user", content: "What is JavaScript?" },
+        ],
+      }),
+    });
+
+    const responseJson = await response.json();
+  }
+
+  return (
+    <div className="flex flex-col h-screen">
+      {/* Navigation Bar */}
+      <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
+        <div className="text-xl  font-bold">Job</div>
+        <div>
+          <input
+            type="password"
+            className="border rounded p-1"
+            placeholder="Paste API key here"
+            value={apiKey}
+            onChange={(e) => setApiKey(e.target.value)}
+          />
+        </div>
+      </nav>
+
+      {/* Message History */}
+      <div className="flex-1">
+        <div className="mx-auto w-full max-w-screen-md">
+          {messageHistory.map((message, idx) => (
+            <div key={idx} className="mt-3">
+              <div className="font-bold">{message.role}</div>
+              <div className="text-lg">{message.content}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Message Input Box */}
+      <div>
+        <div className="mx-auto w-full max-w-screen-md flex px-4 pb-4">
+          <textarea
+            value={userMessage}
+            onChange={(e) => setUserMessage(e.target.value)} // 取代 handleTyping
+            className="border rounded-md text-lg p-1 flex-1"
+            rows={1}
+          />
+          <button
+            onClick={sendRequest}
+            className="bg-blue-500 hover:bg-blue-600 border rounded-md text-white text-lg w-20 p-1 ml-2"
+          >
+            Send
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
