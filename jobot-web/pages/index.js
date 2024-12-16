2feb05a49a2d431a8fc1aa508885b82fe6665892
TAYUAN <u860218@gmail.com>
2024-12-16 06:58:01 -0600

    ts04

--- jobot-web/pages/index.js
+++ jobot-web/pages/index.js
@@ -17,7 +17,7 @@
 
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
-import Head from "next/head";
+// import Head from "next/head";
 // import { createParser } from "eventsource-parser";
 
 const SYSTEM_MESSAGE =
@@ -26,43 +26,7 @@ const SYSTEM_MESSAGE =
 export default function Home() {
   const [apiKey, setApiKey] = useState("");
 
-  const [messages, setMessages] = useState([
-    { role: "system", content: SYSTEM_MESSAGE },
-  ]);
-
-  const [userMessage, setUserMessage] = useState("");
-
   const API_URL = "https://api.openai.com/v1/chat/completions";
-
-  async function sendRequest() {
-    // update the message history
-    const newMessage = { role: "user", content: userMessage };
-
-    const newMessages = [...messages, newMessage];
-
-    setMessages(newMessages);
-    setUserMessage("");
-
-    const response = await fetch(API_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + apiKey,
-      },
-      body: JSON.stringify({
-        model: "gpt-3.5-turbo",
-        messages: newMessages,
-      }),
-    });
-
-    const responseJson = await response.json();
-
-    const newBotMessage = responseJson.choices[0].message;
-
-    const newMessages2 = [...newMessages, newBotMessage];
-
-    setMessages(newMessages2);
-  }
 
   // const sendRequest = async () => {
   //   const updatedMessages = [
@@ -230,9 +194,6 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>Jobot</title>
-      </Head>
       <div className="flex flex-col h-screen">
         {/* Navigation Bar */}
         <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
@@ -248,81 +209,46 @@ export default function Home() {
           </div>
         </nav>
 
-        {/* Message History */}
-        <div className="flex-1  overflow-y-scroll">
-          <div className="mx-auto w-full max-w-screen-md">
-            {messages
-              .filter((message) => message.role !== "system")
-              .map((message, idx) => (
-                <div key={idx} className="my-3">
-                  <div className="font-bold">
-                    {message.role === "user" ? "You" : "Jobot"}
-                  </div>
-                  <div className="text-lg prose">
-                    <ReactMarkdown>{message.content}</ReactMarkdown>
-                  </div>
+        <div class="lg:w-1/2 2xl:w-1/3 p-8 rounded-md bg-gray-100">
+          <h1 class="text-3xl font-bold mb-6">
+            Streaming OpenAI API Completions in JavaScript
+          </h1>
+          <div id="resultContainer" class="mt-4 h-48 overflow-y-auto">
+            <p class="text-gray-500 text-sm mb-2">
+              {
+                <div className="text-lg prose">
+                  <ReactMarkdown>{res}</ReactMarkdown>
                 </div>
-              ))}
+              }
+            </p>
+            <p id="resultText" class="whitespace-pre-line"></p>
           </div>
-        </div>
-        {/* Message Input Box */}
-        <div>
-          <div className="mx-auto w-full max-w-screen-md flex px-4 pb-4">
-            <textarea
-              value={userMessage}
-              onChange={(e) => setUserMessage(e.target.value)} // 取代 handleTyping
-              className="border rounded-md text-lg p-1 flex-1"
-              rows={1}
-            />
+          <input
+            type="text"
+            id="promptInput"
+            class="w-full px-4 py-2 rounded-md bg-gray-200 placeholder-gray-500 focus:outline-none mt-4"
+            placeholder="Enter prompt..."
+            value={promptInput}
+            onChange={(e) => setPromptInput(e.target.value)}
+          />
+          <div class="flex justify-center mt-4">
             <button
-              onClick={sendRequest}
-              className="bg-blue-500 hover:bg-blue-600 border rounded-md text-white text-lg w-20 p-1 ml-2"
+              id="generateBtn"
+              class="w-1/2 px-4 py-2 rounded-md bg-black text-white hover:bg-gray-900 focus:outline-none mr-2 disabled:opacity-75 disabled:cursor-not-allowed"
+              disabled={!isDisable}
+              onClick={generate}
             >
-              Send
+              Generate
+            </button>
+            <button
+              id="stopBtn"
+              disabled={isDisable}
+              class="w-1/2 px-4 py-2 rounded-md border border-gray-500 text-gray-500 hover:text-gray-700 hover:border-gray-700 focus:outline-none ml-2 disabled:opacity-75 disabled:cursor-not-allowed"
+              onClick={stop}
+            >
+              Stop
             </button>
           </div>
-        </div>
-      </div>
-
-      <div class="lg:w-1/2 2xl:w-1/3 p-8 rounded-md bg-gray-100">
-        <h1 class="text-3xl font-bold mb-6">
-          Streaming OpenAI API Completions in JavaScript
-        </h1>
-        <div id="resultContainer" class="mt-4 h-48 overflow-y-auto">
-          <p class="text-gray-500 text-sm mb-2">
-            {
-              <div className="text-lg prose">
-                <ReactMarkdown>{res}</ReactMarkdown>
-              </div>
-            }
-          </p>
-          <p id="resultText" class="whitespace-pre-line"></p>
-        </div>
-        <input
-          type="text"
-          id="promptInput"
-          class="w-full px-4 py-2 rounded-md bg-gray-200 placeholder-gray-500 focus:outline-none mt-4"
-          placeholder="Enter prompt..."
-          value={promptInput}
-          onChange={(e) => setPromptInput(e.target.value)}
-        />
-        <div class="flex justify-center mt-4">
-          <button
-            id="generateBtn"
-            class="w-1/2 px-4 py-2 rounded-md bg-black text-white hover:bg-gray-900 focus:outline-none mr-2 disabled:opacity-75 disabled:cursor-not-allowed"
-            disabled={!isDisable}
-            onClick={generate}
-          >
-            Generate
-          </button>
-          <button
-            id="stopBtn"
-            disabled={isDisable}
-            class="w-1/2 px-4 py-2 rounded-md border border-gray-500 text-gray-500 hover:text-gray-700 hover:border-gray-700 focus:outline-none ml-2 disabled:opacity-75 disabled:cursor-not-allowed"
-            onClick={stop}
-          >
-            Stop
-          </button>
         </div>
       </div>
     </>
