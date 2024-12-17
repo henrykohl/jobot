9c027262e7b55ab158dd5af781e5d9a5c4623865
TAYUAN <u860218@gmail.com>
2024-12-17 02:19:14 -0600

    ts08

--- jobot-web/pages/index.js
+++ jobot-web/pages/index.js
@@ -140,6 +140,7 @@ export default function Home() {
       let msg_stream = "";
       setRes(msg_stream);
 
+      /* eslint-disable no-constant-condition */
       while (true) {
         const { done, value } = await reader.read();
         if (done) {
